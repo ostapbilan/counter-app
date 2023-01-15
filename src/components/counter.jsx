0b356fdb26361c7e42f67468c46fcb33fe81668e
Ostap_BILAN <ostap.bilan@gmail.com>
2023-01-15 12:25:28 +0200

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value,
+    tags: ["tag1", "tag2", "tag3", "tag4"],
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ value: this.state.value - 1 });
+  };
+
+  handleReset = () => {
+    this.setState({ value: 0 });
+  };
+
+  render() {
+    return (
+      <div>
+        {/* <h4>{this.props.children}</h4> */}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+
+        <button
+          onClick={this.handleDecrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Decrement
+        </button>
+
+        <button
+          onClick={this.handleReset}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          Reset
+        </button>
+
+        {/* <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul> */}
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
